11bfdaea10c3258b6e632517cef2e06ce39da835
Tomasz Nurkiewicz <tomasznu@monday.com>
2026-08-26 13:43:59 +0200

    opcodes constants

---
+++ cmd/jvm-compiler/opcodes.go
@@ -0,0 +1,17 @@
+package main
+
+const (
+	opcodeIconstM1      = 0x02
+	opcodeIconst0       = 0x03
+	opcodeBipush        = 0x10
+	opcodeSipush        = 0x11
+	opcodeLdc           = 0x12
+	opcodeLdcW          = 0x13
+	opcodeIadd          = 0x60
+	opcodeIsub          = 0x64
+	opcodeImul          = 0x68
+	opcodeIdiv          = 0x6c
+	opcodeGetstatic     = 0xb2
+	opcodeInvokevirtual = 0xb6
+	opcodeReturn        = 0xb1
+)
